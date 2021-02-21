4a95e4c935d39292038a86daea917d8bffb58f38
Charles Samuel R <51647212+charlescsr@users.noreply.github.com>
2021-02-21 13:50:33 +0000

    Setup site

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask #Anything else from flask can be added here
+from flask import Flask, render_template #Anything else from flask can be added here
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+@app.route('/create')
+def create():
+    return render_template("create.html")
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
